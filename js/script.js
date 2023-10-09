919085878edc50c575966be7a00e407d70efe947
Cristian Jonhson Alvarez <cristian.jonhson@inacapmail.cl>
2023-10-09 11:41:41 -0300

    integracion de Arrays y objetos

--- js/script.js
+++ js/script.js
@@ -1,36 +1,56 @@
 function calcularCosto() {
+    const productos = [{
+            nombre: "Producto 1",
+            precio: 10
+        },
+        {
+            nombre: "Producto 2",
+            precio: 20
+        },
+        {
+            nombre: "Producto 3",
+            precio: 15
+        },
+    ];
+
+    const servicios = [{
+            nombre: "Servicio 1",
+            precio: 30
+        },
+        {
+            nombre: "Servicio 2",
+            precio: 25
+        },
+    ];
+
     while (true) {
-        const producto = prompt("Seleccione un producto o servicio:\n1 - Producto 1 ($10)\n2 - Producto 2 ($20)\n3 - Producto 3 ($15)\n4 - Servicio 1 ($30)\n5 - Servicio 2 ($25)\n\nPara salir, escriba 'salir'");
-        if (producto.toLowerCase() === 'salir') {
+        const seleccion = prompt(
+            "Seleccione un producto o servicio:\n" +
+            productos.map((producto, index) => `${index + 1} - ${producto.nombre} ($${producto.precio})`).join("\n") +
+            "\n" +
+            servicios.map((servicio, index) => `${productos.length + index + 1} - ${servicio.nombre} ($${servicio.precio})`).join("\n") +
+            "\nPara salir, escriba 'salir'"
+        );
+
+        if (seleccion.toLowerCase() === "salir") {
             alert("Gracias por usar el simulador.");
-            break; // Salir del bucle si el usuario escribe 'salir'
+            break;
         }
+
         const cantidad = parseInt(prompt("Ingrese la cantidad:"));
         let costoTotal = 0;
 
-        // Aplicar un condicional para calcular el costo total según la selección
-        switch (producto) {
-            case "1":
-                costoTotal = 10 * cantidad;
-                break;
-            case "2":
-                costoTotal = 20 * cantidad;
-                break;
-            case "3":
-                costoTotal = 15 * cantidad;
-                break;
-            case "4":
-                costoTotal = 30 * cantidad;
-                break;
-            case "5":
-                costoTotal = 25 * cantidad;
-                break;
-            default:
-                alert("Opción no válida.");
-                continue; // Volver al inicio del bucle si la opción no es válida
+        const opcion = parseInt(seleccion);
+        if (!isNaN(opcion) && opcion >= 1 && opcion <= productos.length + servicios.length) {
+            if (opcion <= productos.length) {
+                costoTotal = productos[opcion - 1].precio * cantidad;
+            } else {
+                costoTotal = servicios[opcion - productos.length - 1].precio * cantidad;
+            }
+            alert(`El costo total es: $${costoTotal}`);
+        } else {
+            alert("Opción no válida.");
         }
-
-        alert(`El costo total es: $${costoTotal}`);
     }
 }
 
